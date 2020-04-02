6e5d91dafe1b37609230ace98e18ba8d85bb8542
rdfariz <rdfariz@student.telkomuniversity.ac.id>
2020-04-02 21:00:20 +0700

    Adding Material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, Avatar, Typography, Paper, Grid, Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const message = `Truncation should be conditionally applicable on this long line of text
+ as this is a much longer line than what the container can support. `;
 
 function App() {
+  const classes = useStyles();
+
+  function PaperUser() {
+    return (
+      <Paper className={classes.paper}>
+        <Grid container wrap="wrap" spacing={3}>
+          <Grid item>
+            <Avatar>W</Avatar>
+          </Grid>
+          <Grid item xs>
+            <Typography noWrap>{message}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    )
+  }
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>xs=12</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={3} justify="center">
+          <Grid container item xs={12} spacing={3}>
+            <FormRow />
+          </Grid>
+          <Grid container item xs={12} spacing={3}>
+            <FormRow />
+          </Grid>
+          <Grid container item xs={12} spacing={3}>
+            <FormRow />
+          </Grid>
+        </Grid>
+
+        <PaperUser/>
+
+      </Container>
     </div>
   );
 }
